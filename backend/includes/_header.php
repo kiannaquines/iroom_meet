6ad280114c507b59ad46afeb7b5cdfb9e828d8ca
kiannaquines <kjgnaquines@gmail.com>
2025-05-30 03:15:43 +0800

    refactor: update header navigation icons for Adviser and Parent pages to reflect active state

--- backend/includes/_header.php
+++ backend/includes/_header.php
@@ -1,15 +1,47 @@
 <div class="d-flex justify-content-around my-3">
-    <a href="AdviserGrades.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/grade_icon.png" width="60" alt="Grades" /><br>Grades
+  <a href="AdviserGrades.php" class="text-center text-decoration-none text-dark">
+    <?php
+    $isEnrolleesPage = (basename($_SERVER['PHP_SELF']) === 'AdviserGrades.php');
+    if ($isEnrolleesPage) {
+      echo '<img src="Pictures/grades_green.png" width="60" alt="Grades" />';
+    } else {
+      echo '<img src="Pictures/grade_icon.png" width="60" alt="Grades" />';
+    }
+    ?>
+    <br>Grades
   </a>
   <a href="AdviserAttendance.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/attendance_green.png" width="60" alt="Attendance" /><br>Attendance
+    <?php
+    $isAttendancePage = (basename($_SERVER['PHP_SELF']) === 'AdviserAttendance.php');
+    if ($isAttendancePage) {
+      echo '<img src="Pictures/attendance_green.png" width="60" alt="Attendance" />';
+    } else {
+      echo '<img src="Pictures/attendance_icon.png" width="60" alt="Attendance" />';
+    }
+    ?>
+    <br>Attendance
   </a>
   <a href="AdviserWeeklyPerformance.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/weeklyperformance_icon.png" width="60" alt="Performance" /><br>Weekly<br>Performance
+    <?php
+    $isAttendancePage = (basename($_SERVER['PHP_SELF']) === 'AdviserWeeklyPerformance.php');
+    if ($isAttendancePage) {
+      echo '<img src="Pictures/weeklyperformance_green.png" width="60" alt="Performance" />';
+    } else {
+      echo '<img src="Pictures/weeklyperformance_icon.png" width="60" alt="Performance" />';
+    }
+    ?>
+    <br>Weekly<br>Performance
   </a>
   <a href="AdviserEnrollees.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/enrollees_icon.png" width="60" alt="Adviser" /><br>Enrollees
+    <?php
+    $isEnrolleesPage = (basename($_SERVER['PHP_SELF']) === 'AdviserEnrollees.php');
+    if ($isEnrolleesPage) {
+      echo '<img src="Pictures/enrollees_green.png" width="60" alt="Enrollees" />';
+    } else {
+      echo '<img src="Pictures/enrollees_icon.png" width="60" alt="Enrollees" />';
+    }
+    ?>
+    <br>Enrollees
   </a>
   <a href="Logout.php" class="text-center text-decoration-none text-dark">
     <img src="Pictures/logout_icon.png" width="60" alt="Log Out" /><br>Log Out
--- backend/includes/_header_parent.php
+++ backend/includes/_header_parent.php
@@ -1,15 +1,46 @@
 <div class="d-flex justify-content-around my-3">
-    <a href="ParentGrades.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/grade_icon.png" width="60" alt="Grades" /><br>Grades
+  <a href="ParentGrades.php" class="text-center text-decoration-none text-dark">
+    <?php
+    $isAttendancePage = (basename($_SERVER['PHP_SELF']) === 'ParentGrades.php');
+    if ($isAttendancePage) {
+      echo '<img src="Pictures/grades_green.png" width="60" alt="Grades" />';
+    } else {
+      echo '<img src="Pictures/grade_icon.png" width="60" alt="Grades" />';
+    }
+    ?><br>Grades
   </a>
   <a href="ParentAttendance.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/attendance_green.png" width="60" alt="Attendance" /><br>Attendance
+    <?php
+    $isAttendancePage = (basename($_SERVER['PHP_SELF']) === 'ParentAttendance.php');
+    if ($isAttendancePage) {
+      echo '<img src="Pictures/attendance_green.png" width="60" alt="Attendance" />';
+    } else {
+      echo '<img src="Pictures/attendance_icon.png" width="60" alt="Attendance" />';
+    }
+    ?>
+    <br>Attendance
   </a>
   <a href="ParentWeeklyPerformance.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/weeklyperformance_icon.png" width="60" alt="Performance" /><br>Weekly<br>Performance
+    <?php
+    $isWeeklyPerformancePage = (basename($_SERVER['PHP_SELF']) === 'ParentWeeklyPerformance.php');
+    if ($isWeeklyPerformancePage) {
+      echo '<img src="Pictures/weeklyperformance_green.png" width="60" alt="Performance" />';
+    } else {
+      echo '<img src="Pictures/weeklyperformance_icon.png" width="60" alt="Performance" />';
+    }
+    ?>
+    <br>Weekly<br>Performance
   </a>
   <a href="ParentAdviser.php" class="text-center text-decoration-none text-dark">
-    <img src="Pictures/adviser_icon.png" width="60" alt="Adviser" /><br>Adviser
+    <?php
+    $isAdviserPage = (basename($_SERVER['PHP_SELF']) === 'ParentAdviser.php');
+    if ($isAdviserPage) {
+      echo '<img src="Pictures/adviser_green.png" width="60" alt="Adviser" />';
+    } else {
+      echo '<img src="Pictures/adviser_icon.png" width="60" alt="Adviser" />';
+    }
+    ?>
+    <br>Adviser
   </a>
   <a href="Logout.php" class="text-center text-decoration-none text-dark">
     <img src="Pictures/logout_icon.png" width="60" alt="Log Out" /><br>Log Out
